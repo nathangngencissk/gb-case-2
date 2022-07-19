380b619b72c373f111a0c84cae6e55d8d42ac70b
Nathan G N Gencissk <nathangngencissk@gmail.com>
2022-07-19 17:59:02 -0300

    format code with black

--- dags/import_base_files_to_bq_dag.py
+++ dags/import_base_files_to_bq_dag.py
@@ -1,28 +1,28 @@
 from datetime import datetime
 
-from airflow import models
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow import DAG
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import (
+    GCSToBigQueryOperator,
+)
 
 DAG_ID = "import_base_files_to_bq"
-
 TABLE_NAME = "base_vendas"
-
 DATASET_NAME = "base_vendas_dataset"
-
 PROJECT_ID = "cool-academy-356419"
 
-with models.DAG(
-    dag_id=DAG_ID,
-    schedule_interval='@once',
-    start_date=datetime(2022, 1, 1),
-    catchup=False,
+with DAG(
+        dag_id=DAG_ID,
+        schedule_interval="@once",
+        start_date=datetime(2022, 1, 1),
+        description="Full load of csv files DAG",
+        catchup=False,
 ) as dag:
     load_csv = GCSToBigQueryOperator(
-        task_id='load_csv',
-        bucket='gb-case-2',
-        source_objects=['base2017.csv', 'base2018.csv', 'base2019.csv'],
+        task_id="load_csv",
+        bucket="gb-case-2",
+        source_objects=["base2017.csv", "base2018.csv", "base2019.csv"],
         destination_project_dataset_table=f"{DATASET_NAME}.{TABLE_NAME}",
         skip_leading_rows=1,
         location="us-east4",
-        write_disposition='WRITE_TRUNCATE',
+        write_disposition="WRITE_TRUNCATE",
     )
--- dags/scrap_twitter_dag.py
+++ dags/scrap_twitter_dag.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from airflow import models
+from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
@@ -10,9 +10,11 @@ DAG_ID = "scrap_twitter"
 TABLE_ID = "cool-academy-356419.base_vendas_dataset.tweets_linha_dez_2019"
 SECRET_ID = "TWITTER_TOKEN"
 
+
 def get_twitter_token(**kwargs):
     secrets_hook = SecretsManagerHook(delegate_to=None)
     return secrets_hook.get_secret(secret_id=SECRET_ID)
+
 
 def load_data(**kwargs):
     import json
@@ -24,7 +26,6 @@ def load_data(**kwargs):
     tweets = ti.xcom_pull(task_ids="consume_api")
 
     hook = BigQueryHook(delegate_to=None, use_legacy_sql=False, location="us-east4")
-
     client = hook.get_client()
 
     dataframe = pd.DataFrame(json.loads(tweets)["data"])
@@ -32,10 +33,7 @@ def load_data(**kwargs):
     job_config = bigquery.job.LoadJobConfig()
     job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
 
-    job = client.load_table_from_dataframe(
-        dataframe, TABLE_ID, job_config=job_config
-    )
-
+    job = client.load_table_from_dataframe(dataframe, TABLE_ID, job_config=job_config)
     job.result()
 
 
@@ -43,31 +41,31 @@ def get_linha():
     hook = BigQueryHook(delegate_to=None, use_legacy_sql=False, location="us-east4")
     conn = hook.get_conn()
     cursor = conn.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
         SELECT    LINHA
         FROM      `base_vendas_dataset.agg_vendas_linha_ano_mes`
         WHERE     MONTH = '12'
         AND       YEAR = '2019'
         ORDER BY  TOTAL_SALES DESC
         LIMIT     1;
-    """)
+    """
+    )
     result = cursor.fetchall()
     return result[0][0]
 
-with models.DAG(
-    dag_id=DAG_ID,
-    schedule_interval="@once",
-    start_date=datetime(2022, 1, 1),
-    catchup=False,
+
+with DAG(
+        dag_id=DAG_ID,
+        schedule_interval="@once",
+        start_date=datetime(2022, 1, 1),
+        description="Scrap twitter DAG",
+        catchup=False,
 ) as dag:
-    get_linha_task = PythonOperator(
-        task_id="get_linha",
-        python_callable=get_linha
-    )
+    get_linha_task = PythonOperator(task_id="get_linha", python_callable=get_linha)
 
     get_twitter_token_task = PythonOperator(
-        task_id="get_twitter_token",
-        python_callable=get_twitter_token
+        task_id="get_twitter_token", python_callable=get_twitter_token
     )
 
     consume_api_task = SimpleHttpOperator(
@@ -76,17 +74,17 @@ with models.DAG(
         data={
             "query": "Boticário {{ ti.xcom_pull(task_ids='get_linha') }} lang:pt",
             "max_results": 50,
-            "tweet.fields": "author_id,created_at"
+            "tweet.fields": "author_id,created_at",
         },
         method="GET",
         endpoint="/2/tweets/search/recent",
-        headers={"Authorization": "Bearer {{ ti.xcom_pull(task_ids='get_twitter_token') }}"}
+        headers={
+            "Authorization": "Bearer {{ ti.xcom_pull(task_ids='get_twitter_token') }}"
+        },
     )
 
     load_data_task = PythonOperator(
-        task_id="load_data",
-        python_callable=load_data,
-        provide_context=True
+        task_id="load_data", python_callable=load_data, provide_context=True
     )
 
     [get_linha_task, get_twitter_token_task] >> consume_api_task >> load_data_task
